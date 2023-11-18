bf1943067797eecc4a535b43c905724f772a5deb
brandontran1220 <brandontran1220@gmail.com>
2023-11-17 21:01:05 -0800

    Added function definitions for taskList, added unit tests for taskList, and some modifications to task

--- headers/taskList.h
+++ headers/taskList.h
@@ -1,21 +1,41 @@
-#include "../header/task.h"
+#ifndef TASKLIST_H
+#define TASKLIST_H
+
+#include "task.h"
+#include <vector>
 
 class TaskList {
  public:
-    TaskList();
-    ~TaskList();
-    void editListName(string& name); // Edit list's name
-    void editListDescription(string& description); // Edit list's description
-    void editListClassification(string& classification); // Edit list's classification
-    void markComplete(); // Mark if list has completed all tasks
+    TaskList(); // Default Constructor
+    // ~TaskList(); // Destructor
+    // TaskList(const TaskList&); // Copy Constructor
+    // TaskList& operator=(const TaskList&); // Assignment operator
+
+    // setters
+    void editListName(const string& newName); // Edit list's name
+    void editListDescription(const string& newDescription); // Edit list's description
+    void editListClassification(const string& newClassification); // Edit list's classification
+    void switchCompleteStatus(); // Change the completion status of the list
     void addTask(Task task); // Add task to list
     void removeTask(int index); // Remove a specific task from list
-    void viewTasks(); // Display all tasks within list
     void editTask(int index); // Edit a specific task within the test
+    void findCompletedTasks(); // Count the number of completed tasks
+
+    // getters
+    void viewTasks() const; // Display all tasks within list
+    string getListName() const; // Return the list's name
+    string getListDescription() const; // Return the list's description
+    string getListClassification() const; // Return the list's classification
+    bool getListCompleteStatus() const; // Return the completion status of the list
+    double getProgress() const; // Return the progress of completed tasks to total tasks
+    
  private: 
     vector<Task> userList;
     string name;
     string description;
     string classification;
     bool complete;
+    double numOfCompleted;
 };
+
+#endif
--- src/taskList.cpp
+++ src/taskList.cpp
@@ -1,1 +1,73 @@
-#include "../header/taskList.h"
+#include "../headers/taskList.h"
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+TaskList::TaskList() : name(""), description(""), classification(""), complete(false), numOfCompleted(0) {}
+
+// TaskList::~TaskList() { NOT SURE IF NEEDED
+
+// }
+
+void TaskList::editListName(const string& newName) {
+    name = newName;
+}
+
+void TaskList::editListDescription(const string& newDescription) {
+    description = newDescription;
+}
+
+void TaskList::editListClassification(const string& newClassification) {
+    classification = newClassification;
+}
+
+void TaskList::switchCompleteStatus() {
+    if (complete == true) complete = false;
+    else complete = true;
+}
+
+void TaskList::addTask(const Task task) {
+    userList.push_back(task);
+}
+
+void TaskList::removeTask(int index) {
+    userList.erase(userList.begin() + index);
+}
+
+void TaskList::editTask(int index) {
+    // Waiting for prompt to be able to edit that certain task
+}
+
+void TaskList::findCompletedTasks() {
+    for(int i = 0; i < userList.size(); ++i) {
+        if (userList.at(i).getCompleteStatus() == true) {
+            ++numOfCompleted;
+        }
+    }
+}
+
+void TaskList::viewTasks() const {
+    // Going to call prompt NEED TO WAIT
+}
+
+string TaskList::getListName() const {
+    return name;
+}
+
+string TaskList::getListDescription() const {
+    return description;
+}
+
+string TaskList::getListClassification() const {
+    return classification;
+}
+
+bool TaskList::getListCompleteStatus() const {
+    return complete;
+}
+
+double TaskList::getProgress() const {
+    return numOfCompleted / userList.size();
+}
