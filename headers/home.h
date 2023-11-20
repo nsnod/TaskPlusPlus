5ecfce8f708dfc252d7a19a580e5c80f57cec53e
NoahSnod <nsnod001@ucr.edu>
2023-11-19 18:33:29 -0800

    Built home.cpp and home.h and implemented the createNewTask feature. Also added to .gitignore

--- headers/home.h
+++ headers/home.h
@@ -1,5 +1,9 @@
+#ifndef HOME_H
+#define HOME_H
+
 #include "../headers/task.h"
 #include "../headers/taskList.h"
+#include "../headers/task.h"
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
@@ -17,3 +21,5 @@ class Home {
    unordered_map<string, unordered_set<TaskList*>> classificationBasedStorage;
    unordered_map<string, Task*> soloTasks;
 };
+
+#endif
--- src/home.cpp
+++ src/home.cpp
@@ -1,4 +1,6 @@
 #include "../headers/home.h"
+#include "../headers/taskList.h"
+#include "../headers/task.h"
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
@@ -7,7 +9,14 @@
 using namespace std;
 
 void Home::createNewTask(vector<string> inputs) {
-    Task newTask;
+    Task* newTask = new Task;
+
+    newTask->setName(inputs[0]);
+    newTask->setPriority(inputs[1]);
+    newTask->setFullDueDate(inputs[2]);
+    newTask->setFullAssignedDate(inputs[3]);
+    newTask->setDescription(inputs[4]);
+    newTask->setList(inputs[5]);
 }
 
 void Home::createNewList(vector<string> inputs) {
