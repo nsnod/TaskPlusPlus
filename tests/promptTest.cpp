e060fe15ebd65d57180908a061cc300b36ba30c1
rgujr001 <rgujr001@ucr.edu>
2023-11-28 12:25:29 -0800

    updating promptest to test tasklist prompt

--- tests/promptTest.cpp
+++ tests/promptTest.cpp
@@ -2,13 +2,34 @@
 #include "gtest/gtest.h"
 
 TEST(promptTest, printMenu){
-    prompt p;
+    Prompt p;
 
     //testing the mutator for prompt
-    EXPECT_NO_THROW(p.setSelection());
+    EXPECT_NO_THROW(p.SetSelection());
 
     //testing that printing main menu doesnt cause an error
-    
     EXPECT_NO_THROW(p.printMainMenu());
 
+}
+
+TEST(promptTest, newListPrompt){
+    Prompt p;
+
+    EXPECT_NO_THROW(p.newListPrompt());
+}
+
+TEST(promptTest, newTaskPrompt){
+
+    Prompt p;
+
+    EXPECT_NO_THROW(p.newTaskPrompt());
+    
+}
+
+TEST(promptTest, taskEditorPrompt){
+
+    Prompt p;
+    
+    EXPECT_NO_THROW(p.taskEditorPrompt());
+    
 }
