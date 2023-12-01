0e4f7a67feb25642e78a6b8a765426de2bc8f7ca
rgujr001 <rgujr001@ucr.edu>
2023-11-30 18:06:25 -0800

    starting developing main function

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+//#pragma once
+#include <iostream>
+
+#include "../headers/home.h"
+#include "../headers/prompt.h"
+#include "../headers/archive.h"
+#include "../headers/recommender.h"
+#include "../headers/task.h"
+#include "../headers/taskList.h"
+#include "../headers/upcoming.h"
+#include "../headers/view.h"
+#include "../headers/weekly.h"
+
+using namespace std;
+
+int main(){
+
+    prompt Prompt;
+    Home Home;
+
+    //Prompt.printMainMenu();
+
+    return 0;
+}
