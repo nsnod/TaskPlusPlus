39f8a841144ee10df652f514d2a9a2f12c1b8b07
NoahSnod <nsnod001@ucr.edu>
2023-12-08 02:20:44 -0800

    Fixed sams silly little loop error

--- src/prompt.cpp
+++ src/prompt.cpp
@@ -508,7 +508,7 @@ void prompt::listEditorPrompt(Home* h) {
 
     while(h->findTaskList(userList) == nullptr){
         h->viewLists();
-        cout << "Please enter a valid list";
+        cout << "Please enter a valid list: ";
         getline(cin,userList);
     }
 
@@ -1005,7 +1005,7 @@ void prompt::viewOverall(View* mainView, Home* userHome){
             getline(cin,userList);
             cout << endl;
 
-            while(userHome->findTaskList(userList)){
+            while(userHome->findTaskList(userList) == nullptr){
                 userHome->viewLists();
                 cout << "INVALID please enter existing list: ";
                 getline(cin,userList);
